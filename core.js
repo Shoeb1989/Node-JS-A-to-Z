00a6f9b0657b76e0e375f467def6610fd5d0179d
Shoeb1989 <shoebsikder1@gmail.com>
2025-05-05 12:21:15 +0600

    const and let

--- core.js
+++ core.js
@@ -1,6 +1,31 @@
-var Name = 'max';
-var age = 26;
-var hobby = true;
+// var Name = 'max';
+// var age = 26;
+// var hobby = true;
+
+// function SumarizeUser (UserName, Userage , Userhobby) {
+//     return (
+//         'his name is: ' + 
+//         UserName + 'his age is :  ' + 
+//         Userage + ' his hobby is : ' + Userhobby
+//     );
+// }
+
+// console.log(SumarizeUser(Name , age , hobby));
+
+
+// let Name = 'max';
+// let age = 26;
+// let hobby = true;
+
+age = 30;
+
+// let value can be changeable
+// Const can't be changeable 
+
+
+// const Name = 'max';
+// let age = 26;
+// const hobby = true;
 
 function SumarizeUser (UserName, Userage , Userhobby) {
     return (
