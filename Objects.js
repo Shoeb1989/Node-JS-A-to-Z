f968f4a65687b09a304b8e54639cbc1b6387e271
Shoeb1989 <shoebsikder1@gmail.com>
2025-05-15 12:14:35 +0600

    push method

--- Objects.js
+++ Objects.js
@@ -14,5 +14,7 @@ const hobbies = ['Sports', 'Cooking'];
 // for (let hobby of hobbies) {
 // console. log (hobby);
 
-console.log(hobbies.map(hobby => 'Hobby:'+ hobby));
+// console.log(hobbies.map(hobby => 'Hobby:'+ hobby));
+
+hobbies.push('shoeb');
 console.log(hobbies);
