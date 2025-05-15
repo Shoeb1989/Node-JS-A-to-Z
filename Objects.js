6c52dac2546c83095d85b1e883731edc91ffc35f
Shoeb1989 <shoebsikder1@gmail.com>
2025-05-15 12:09:26 +0600

    Objects

--- Objects.js
+++ Objects.js
@@ -1,11 +1,18 @@
-const person = {
-    Name: 'shoeb',
-    age:  26,
+// const person = {
+//     Name: 'shoeb',
+//     age:  26,
 
-    try(){
-        console.log('hi i am ' + this.Name);
+//     try(){
+//         console.log('hi i am ' + this.Name);
 
-    }
-};
+//     }
+// };
 
-person.try();
+// person.try();
+
+const hobbies = ['Sports', 'Cooking'];
+// for (let hobby of hobbies) {
+// console. log (hobby);
+
+console.log(hobbies.map(hobby => 'Hobby:'+ hobby));
+console.log(hobbies);
